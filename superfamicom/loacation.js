9e097ca9646c95228bb272cd801ff803b530845d
SEIMIN123 <interkp12@gmail.com>
2023-02-07 22:19:17 +0900

    .

--- superfamicom/loacation.js
+++ superfamicom/loacation.js
@@ -1,5 +1,5 @@
 const but = document.querySelector('.button');
 but.addEventListener('click', function(){
-let page = 'https://seimin123.github.io/ASOBI-1/';
+let page = './superfamicom/main.html';
 location.href = page;
 });
